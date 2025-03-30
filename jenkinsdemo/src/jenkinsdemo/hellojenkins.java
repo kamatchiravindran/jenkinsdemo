794c806dbe648c09c7677bc0924f04a7cc9333a2
kamatchimca <kamatchimca@penguin>
2025-03-29 20:38:20 -0400

    First commit

---
+++ jenkinsdemo/src/jenkinsdemo/hellojenkins.java
@@ -0,0 +1,10 @@
+package jenkinsdemo;
+
+public class hellojenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jenkins !!!");
+	}
+
+}
